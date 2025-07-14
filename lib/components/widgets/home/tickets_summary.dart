c8c438c350ca2fe87cd33eb529114bd51823c8e5
cakadi190 <cakadi190@gmail.com>
2025-07-15 06:04:45 +0700

    feat(home): implement tickets summary widget

--- lib/components/widgets/home/tickets_summary.dart
+++ lib/components/widgets/home/tickets_summary.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wifiber/providers/complaint_provider.dart';
+import 'package:wifiber/services/complaint_service.dart';
 
 class TicketSummary extends StatelessWidget {
   const TicketSummary({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8),
-        ),
-      ),
+    return ChangeNotifierProvider(
+      create: (_) => ComplaintProvider(ComplaintService())..fetchComplaints(),
+      child: _TicketSummaryView(),
     );
+  }
+}
+
+class _TicketSummaryView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
