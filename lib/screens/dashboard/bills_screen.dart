f969954b354f52b9ef00085846a4b195ab3f8d61
cakadi190 <cakadi190@gmail.com>
2025-07-14 17:53:08 +0700

    feat(bills): implement basic bills screen layout

--- lib/screens/dashboard/bills_screen.dart
+++ lib/screens/dashboard/bills_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wifiber/components/system_ui_wrapper.dart';
 import 'package:wifiber/config/app_colors.dart';
+import 'package:wifiber/helpers/system_ui_helper.dart';
 
 class BillsScreen extends StatefulWidget {
   const BillsScreen({super.key});
@@ -11,11 +13,26 @@ class BillsScreen extends StatefulWidget {
 class _BillsScreenState extends State<BillsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.primary,
-      appBar: AppBar(title: const Text('Daftar Tagihan')),
-      body: Center(
-        child: Text('Halaman Daftar Tagihan'),
+    return SystemUiWrapper(
+      style: SystemUiHelper.duotone(
+        statusBarColor: AppColors.primary,
+        navigationBarColor: Colors.white,
+      ),
+      child: Scaffold(
+        backgroundColor: AppColors.primary,
+        appBar: AppBar(title: const Text('Daftar Tagihan')),
+        body: SafeArea(child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(24),
+              topRight: Radius.circular(24),
+            ),
+          ),
+          child: const Center(
+            child: Text('Bills'),
+          ),
+        )),
       ),
     );
   }
