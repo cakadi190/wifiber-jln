e6b5aac1332f155409d341a0bf93b467cc9611ec
cakadi190 <cakadi190@gmail.com>
2025-07-19 22:10:54 +0700

    style(profile): set primary color as background

--- lib/screens/dashboard/profile/main_profile_screen.dart
+++ lib/screens/dashboard/profile/main_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wifiber/config/app_colors.dart';
 
 class MainProfileScreen extends StatefulWidget {
   const MainProfileScreen({super.key});
@@ -11,6 +12,7 @@ class _MainProfileScreenState extends State<MainProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.primary,
       appBar: AppBar(
         title: const Text('Profil Saya'),
       ),
